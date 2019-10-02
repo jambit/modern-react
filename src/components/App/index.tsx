b3e0518fe2b53b3cc3474e116be10d68ace16977
Santo Pfingsten <santo.pfingsten@jambit.com>
2019-10-02 16:09:45 +0200

    yaml & image assets

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import './style.scss';
+import data from './data.yml';
+import avatar from './avatar.png';
+
+const typedData: { list: string[] } = data;
 
 export default () => (
     <div className="app-container">
-        <h1>Hello 3</h1>
+        <h1><img src={avatar} alt="Avatar"/> Hello 3</h1>
+        <ul>
+            {typedData.list.map(s => <li key={s}>{s}</li>)}
+        </ul>
     </div>
 );
---
+++ src/global.d.ts
@@ -0,0 +1,8 @@
+declare module '*.yml' {
+    const content: any;
+    export default content;
+}
+declare module '*.png' {
+    const content: string;
+    export default content;
+}
