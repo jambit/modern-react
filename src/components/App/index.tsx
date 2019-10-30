d052401c89a18db9126b2a52b606493af09894c0
Santo Pfingsten <santo.pfingsten@jambit.com>
2019-10-30 15:11:22 +0100

    useReducer

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Stateful from '../Stateful';
+import NotRedux from '../NotRedux';
 
 export default () => (
     <div>
         <Stateful />
+        <NotRedux />
     </div>
 );
