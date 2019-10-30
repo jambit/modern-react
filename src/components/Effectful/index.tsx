cdbf1e4468a6f56a28af8498ee790c6b34183c92
Santo Pfingsten <santo.pfingsten@jambit.com>
2019-10-30 15:17:42 +0100

    useEffect 2

--- src/components/Effectful/index.tsx
+++ src/components/Effectful/index.tsx
@@ -2,9 +2,19 @@ import React, { useEffect, useState } from 'react';
 
 export default () => {
     const [originalTitle] = useState(document.title);
+    const [value, setValue] = useState(false);
+    useEffect(() => {
+        const element = document.getElementById('effect');
+        element.textContent = new Date().toISOString();
+    }, [value]);
     useEffect(() => {
         document.title = 'Effectful React';
         return () => document.title = originalTitle;
     }, []);
-    return <span>Woot</span>;
+    return (
+        <span>
+            <span id="effect">Some Effect</span>
+            <button onClick={() => setValue(!value)}>{value ? 'Foo' : 'Bar'}</button>
+        </span>
+    );
 }
