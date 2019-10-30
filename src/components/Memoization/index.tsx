80525d7bf26a80313565d6f5608791f9573f5ea2
Santo Pfingsten <santo.pfingsten@jambit.com>
2019-10-30 15:27:31 +0100

    useCallback

--- src/components/Memoization/index.tsx
+++ src/components/Memoization/index.tsx
@@ -1,9 +1,12 @@
-import React, { useState, useMemo } from 'react';
+import React, { useCallback, useState, useMemo } from 'react';
 
 export default () => {
     const [a, setA] = useState('0');
     const [b, setB] = useState('0');
     const sum = useMemo(() => parseFloat(a) + parseFloat(b), [a, b]);
+    const onClick = useCallback(() => {
+        alert(`Value: ${sum}`);
+    }, [sum]);
     return (
         <div>
             <input onChange={(e) => setA(e.target.value)} value={a} />
@@ -11,6 +14,7 @@ export default () => {
             <input onChange={(e) => setB(e.target.value)} value={b} />
             =
             {sum}
+            <button onClick={onClick}>Send</button>
         </div>
     );
 }
