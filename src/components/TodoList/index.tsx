cac2b8185f6741db31eb34dc03181f2f5cf75502
Santo Pfingsten <santo.pfingsten@jambit.com>
2019-12-02 14:02:14 +0100

    select todos

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 
 import TodoListEntry from './TodoListEntry';
 import './style.scss';
+import { useSelector } from 'react-redux';
+import { selectTodos } from '../../redux/todos';
 
 export default () => {
+    const todos = useSelector(selectTodos);
     return (
         <ul className="todo-list">
-            <TodoListEntry label="Make cookies" checked={false} />
-            <TodoListEntry label="Go to Christmas party" checked={false} />
-            <TodoListEntry label="Wish everyone a merry Christmas" checked={false} />
-            <TodoListEntry label="Dress up like Santa" checked />
-            <TodoListEntry label="Ignore 1-3" checked />
-            <TodoListEntry label="Steal Christmas" checked />
+            {todos.map((todo) => <TodoListEntry label={todo.label} checked={todo.checked} key={todo.id} />)}
             <li><input className="todo-list-new-entry" placeholder="Add a new entry" /><button>+</button></li>
         </ul>
     );
