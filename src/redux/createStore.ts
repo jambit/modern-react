d847e754f31dd028d191890e10ff17aa8d3c06b5
Santo Pfingsten <santo.pfingsten@jambit.com>
2020-01-06 14:52:31 +0100

    dummy session state

--- src/redux/createStore.ts
+++ src/redux/createStore.ts
@@ -1,9 +1,11 @@
 import { configureStore } from 'deox';
 import { combineReducers } from 'redux';
 import { todosReducer } from './todos/reducer';
+import { sessionReducer } from './session/reducer';
 
 export default () => configureStore({
     reducer: combineReducers({
         todos: todosReducer,
+        session: sessionReducer,
     })
 });
