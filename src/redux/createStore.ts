590f5d159d765b24463af1151aa609e7c20dde10
Santo Pfingsten <santo.pfingsten@jambit.com>
2020-01-06 15:01:22 +0100

    adding saga middleware and an empty root saga

--- src/redux/createStore.ts
+++ src/redux/createStore.ts
@@ -1,11 +1,21 @@
 import { configureStore } from 'deox';
 import { combineReducers } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import { todosReducer } from './todos/reducer';
 import { sessionReducer } from './session/reducer';
+import rootSaga from './rootSaga';
 
-export default () => configureStore({
-    reducer: combineReducers({
-        todos: todosReducer,
-        session: sessionReducer,
-    })
-});
+export default () => {
+    const sagaMiddleware = createSagaMiddleware();
+    const store = configureStore({
+        middleware: [
+            sagaMiddleware
+        ],
+        reducer: combineReducers({
+            todos: todosReducer,
+            session: sessionReducer,
+        })
+    });
+    sagaMiddleware.run(rootSaga);
+    return store;
+};
---
+++ src/redux/rootSaga.ts
@@ -0,0 +1,4 @@
+
+export default function* rootSaga() {
+    // call other sagas from here
+}
