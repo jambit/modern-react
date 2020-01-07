1649e5c88cb0efb7b0a47d3709077f39786ca205
Santo Pfingsten <santo.pfingsten@jambit.com>
2020-01-07 14:11:39 +0100

    adding a router

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import TodoList from '../TodoList';
+import { Router } from 'react-router-ts';
 
-// 1. add routeMatcherFactory
-// 2. add Router
 // 3. Add 2 routes / and /todo with links to eachother
 // 4. make TodoList load Lazily (Suspense + Loading)
 
+function routeMatcherFactory(pattern: string) {
+    return (path: string) => ((path === pattern) ? {} : null);
+}
+
 export default () => (
     <div>
-        <h2>GRINCH TO DO LIST</h2>
-        <TodoList />
+        <Router routeMatcherFactory={routeMatcherFactory}>
+            <h2>GRINCH TO DO LIST</h2>
+            <TodoList />
+        </Router>
     </div>
 );
