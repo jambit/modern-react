842a61b86fe330cbf450fdf04534863ff4a20c63
Santo Pfingsten <santo.pfingsten@jambit.com>
2020-01-07 15:11:53 +0100

    missed a todo cleanup

--- src/redux/createStore.ts
+++ src/redux/createStore.ts
@@ -2,7 +2,6 @@ import { createStore } from 'redux-dynamic-modules';
 import { getSagaExtension } from 'redux-dynamic-modules-saga';
 import coreModule from './core/module';
 
-// 3. remove todoModule in favor of DynamicModuleLoader
 export default () => createStore(
     {
         initialState: {},
