2cd88c3c09de417a6526ebfb4f03a8b2315e17ac
Santo Pfingsten <santo.pfingsten@jambit.com>
2020-01-07 14:18:21 +0100

    lazy todo list loading

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import TodoList from '../TodoList';
+import React, { Suspense } from 'react';
 import { Router, Route, Link } from 'react-router-ts';
-
-// 4. make TodoList load Lazily (Suspense + Loading)
+import Loading from '../Loading';
 
 function routeMatcherFactory(pattern: string) {
     return (path: string) => ((path === pattern) ? {} : null);
 }
+
+const LazyTodoList = React.lazy(() => import('../TodoList/index'));
 
 export default () => (
     <div>
         <Router routeMatcherFactory={routeMatcherFactory}>
             <Route path="/todo">
                 <h2>GRINCH TO DO LIST</h2>
-                <TodoList />
+                <Suspense fallback={<Loading />}>
+                    <LazyTodoList />
+                </Suspense>
                 <Link href="/">Home</Link>
             </Route>
             <Route path="/">
