8d7c28365ee453e5fda9faeb58e78af69fa31b4d
Santo Pfingsten <santo.pfingsten@jambit.com>
2020-06-02 17:22:48 +0200

    skeleton for the testing workshop

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,18 +1,4 @@
 import React from 'react';
-import Stateful from '../Stateful';
-import NotRedux from '../NotRedux';
-import Contextual, { MyContext } from '../Contextual';
 import Memoization from '../Memoization';
-import Reference from '../Reference';
 
-export default () => (
-    <div>
-        <Stateful />
-        <NotRedux />
-        <MyContext.Provider value="someOtherValue">
-            <Contextual />
-        </MyContext.Provider>
-        <Memoization />
-        <Reference />
-    </div>
-);
+export default () => <Memoization />;
