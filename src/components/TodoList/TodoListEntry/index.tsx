b4e3991e3cf07ef9db9c445486bc01b23fb4ed70
Santo Pfingsten <santo.pfingsten@jambit.com>
2021-03-17 17:00:45 +0100

    solution for react-redux

--- src/components/TodoList/TodoListEntry/index.tsx
+++ src/components/TodoList/TodoListEntry/index.tsx
@@ -4,14 +4,22 @@ import './style.scss';
 interface TodoListEntryProps {
     label: string;
     checked: boolean;
+    onToggle: () => void;
+    onRemove: () => void;
 }
 
-export default ({ label, checked }: TodoListEntryProps) => {
+export default ({ label, checked, onToggle, onRemove }: TodoListEntryProps) => {
     return (
         <li className={`todo-list-entry${checked ? ' checked' : ''}`}>
-            <span className="icon">{checked ? '☑' : '☐'}</span>{' '}
-            <span className="label">{label}</span>
-            <button className="remove">x</button>
+            <span className="icon" onClick={onToggle}>
+                {checked ? '☑' : '☐'}
+            </span>{' '}
+            <span className="label" onClick={onToggle}>
+                {label}
+            </span>
+            <button className="remove" onClick={onRemove}>
+                x
+            </button>
         </li>
     );
 };
