ab4e013496baa4e63d8edf416339945489e39361
Santo Pfingsten <santo.pfingsten@jambit.com>
2021-03-17 16:59:32 +0100

    start for react-redux

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -3,19 +3,6 @@ import React from 'react';
 import TodoListEntry from './TodoListEntry';
 import './style.scss';
 
-interface TodoItem {
-    id: number;
-    label: string;
-    checked: boolean;
-}
-
-const initialState = {
-    nextId: 0,
-    items: [] as TodoItem[],
-};
-
-type TodoState = typeof initialState;
-
 export default () => {
     const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
