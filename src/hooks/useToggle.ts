6fee6d85b8fa2368f87a44529a988b0723005be2
Santo Pfingsten <santo.pfingsten@jambit.com>
2021-10-21 10:21:28 +0200

    small improvement

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -2,6 +2,6 @@ import { useState, useCallback } from 'react';
 
 export default (initialValue: boolean) => {
     const [value, setValue] = useState(initialValue);
-    const toggle = useCallback(() => setValue(!value), [value]);
+    const toggle = useCallback(() => setValue((lastValue) => !lastValue), []);
     return [value, toggle] as const;
 };
